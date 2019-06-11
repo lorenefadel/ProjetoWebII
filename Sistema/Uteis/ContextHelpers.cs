403f1b64a0eadca61d3b0032f8c1c8e8e547cb6b
Lorene Fadel <fadellorene@gmail.com>
2019-06-10 21:11:34 -0300

    commit

--- Sistema/Uteis/ContextHelpers.cs
+++ Sistema/Uteis/ContextHelpers.cs
@@ -9,18 +9,22 @@ namespace Sistema.Uteis
 {
     public static class ContextHelpers
     {
-        private static string instanceName = @"localhost\SQLEXPRESS";
+        private static string instanceName = @"belchior.database.windows.net,1433";
         private static string databaseName = "sistema_facens";
-        private static string databaseUserName = "sa";
-        private static string databasePassword = "!usr000@";
+        private static string databaseUserName = "belchior";
+        private static string databasePassword = "pk9Su6V3r7KP!db";
 
         public static string connectionString
         {
+            //get
+            //{
+            //    return $"Data Source={instanceName};" +
+            //            $"Initial Catalog={databaseName};" +
+            //            $"Trusted_Connection=True";
+            //}
             get
             {
-                return $"Data Source={instanceName};" +
-                        $"Initial Catalog={databaseName};" +
-                        $"Trusted_Connection=True";
+                return "Server=tcp:belchior.database.windows.net,1433;Initial Catalog=sistema_facens;Persist Security Info=False;User ID=belchior;Password=pk9Su6V3r7KP!db;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             }
         }
 
